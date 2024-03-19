579c69cb3f07c0d90f64ef9dcc55becac161f97f
beatriz <beatrizffeo@gmail.com>
2024-03-19 16:32:22 -0600

    lab complete

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+const titleCase = tutorials.map((tutorial) => {
+  return titleCaseString(tutorial);
+});
+function titleCased() {
+  return titleCase
+}
+
+function titleCaseString(str) {
+  var splitStr = str.split(' ');
+  for (var i = 0; i < splitStr.length; i++) {
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  return splitStr.join(' ');
 }
